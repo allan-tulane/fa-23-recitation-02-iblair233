079355a89a44f18f61cc9b79bc0c6e54a0655d9c
iblair233 <126489949+iblair233@users.noreply.github.com>
2023-09-13 10:19:05 -0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -12,7 +12,7 @@ def test_simple_work():
 def test_work():
 	assert work_calc(10, 2, 2,lambda n: 1) == 31
 	assert work_calc(20, 1, 2, lambda n: n*n) == 533.8125
-	assert work_calc(30, 3, 2, lambda n: n) == 638.62
+	assert work_calc(30, 3, 2, lambda n: n) == 638.625
 	assert work_calc(10, 2, 2, lambda n: n*n) == 203.5
 	assert work_calc(60, 2, 2, lambda n: n*n*n) == 287993.6875
 	assert work_calc(40, 1, 2, lambda n: n+n) == 158.5
