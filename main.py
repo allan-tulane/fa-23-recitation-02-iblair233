e30d6621f400488a325d13679ceea1c1e9efabfa
iblair233 <126489949+iblair233@users.noreply.github.com>
2023-09-13 10:14:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -82,6 +82,27 @@ def compare_work(work_fn1, work_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000])
 			))
 	return result
 
+
+def compare_span(span_fn1, span_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000]):
+	"""
+	Compare the values of different recurrences for 
+	given input sizes.
+
+	Returns:
+	A list of tuples of the form
+	(n, work_fn1(n), work_fn2(n), ...)
+	
+	"""
+	result = []
+	for n in sizes:
+		# compute W(n) using current a, b, f
+		result.append((
+			n,
+			span_fn1,
+			span_fn2
+			))
+	return result
+
 def print_results(results):
 	""" done """
 	print(tabulate.tabulate(results,
